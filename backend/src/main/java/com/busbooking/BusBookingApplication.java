e5f0e10ce60ba2586a94f4f52fb1d63a08aaa8c2
Aarush Mishra <aarushm893@gmail.com>
2026-03-28 14:57:04 +0530

    Fix ERROR 5: Add @NonNull annotation to inherited method
    - Added import: org.springframework.lang.NonNull
    - Added @NonNull annotation to CorsRegistry parameter in addCorsMappings()
    - Fixes: Missing non-null annotation for inherited method override
    - Java 25 compliance: Proper nullness checking annotation

--- backend/src/main/java/com/busbooking/BusBookingApplication.java
+++ backend/src/main/java/com/busbooking/BusBookingApplication.java
@@ -3,6 +3,7 @@ package com.busbooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,7 +26,7 @@ public class BusBookingApplication {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:3000", "http://localhost", "http://127.0.0.1:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
