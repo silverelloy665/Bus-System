3cadeaa38f8998037bc2513c2619f30afc6386c5
Aarush Mishra <aarushm893@gmail.com>
2026-03-29 23:47:25 +0530

    finalfeatures

--- backend/src/main/java/com/busbooking/controller/RouteReviewController.java
+++ backend/src/main/java/com/busbooking/controller/RouteReviewController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/reviews")
 @CrossOrigin(origins = "*")
+@SuppressWarnings("null")
 public class RouteReviewController {
 
     @Autowired
